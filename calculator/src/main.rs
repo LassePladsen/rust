3c329f7b8a37d3060ef29de89dfd7600b11a253b
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-06-13 19:51:38 +0200

    worworking result testing. but uses expect.

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,12 +1,33 @@
 use std::{
     f64,
-    io::{Write, stdin, stdout},
+    io::{self, Write},
     num::{ParseFloatError, ParseIntError},
 };
 
+#[allow(dead_code)]
+fn read_line() -> String {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Could not read line from stdin");
+    String::from(buffer.trim())
+}
+
+#[allow(dead_code)]
+fn read_integer() -> Result<i32, ParseIntError> {
+    let line = read_line();
+    line.parse()
+}
+
+#[allow(dead_code)]
+fn read_float() -> Result<f64, ParseFloatError> {
+    let line = read_line();
+    line.parse()
+}
+
 fn main() {
     print!("Enter a number: $ ");
-    stdout().flush().expect("Could not flush stdout");
+    io::stdout().flush().expect("Could not flush stdout");
     let input = read_float();
     match input {
         Ok(number) => println!("You entered {number}"),
@@ -14,18 +35,3 @@ fn main() {
     }
 }
 
-fn read_line() -> String {
-    let mut buffer = String::new();
-    stdin().read_line(&mut buffer).expect("Failed to read line");
-    String::from(buffer.trim())
-}
-
-#[allow(dead_code)]
-fn read_integer() -> Result<i32, ParseIntError> {
-    read_line().parse()
-}
-
-#[allow(dead_code)]
-fn read_float() -> Result<f64, ParseFloatError> {
-    read_line().parse()
-}
